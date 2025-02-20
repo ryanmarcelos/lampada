17b14a7361bd6fa23592c019e1786ccd03c058c9
RYAN <126591328+ryanmarcelos@users.noreply.github.com>
2025-02-20 09:10:33 -0300

    Update lampada.js

--- IMG/lampada.js
+++ IMG/lampada.js
@@ -3,7 +3,7 @@ const turnOn = document.getElementById('turnOn');
 const turnOff = document.getElementById('turnOff');
 
 function isLampBroken(){
-    return lamp.scr.indexOf('quebrada')> -1
+    return lamp.src.indexOf('quebrada')> -1
 }
 function lampOn(){
     if(!isLampBroken()){
